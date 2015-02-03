0ebad3746a54ddf648a4ca2f571f96a76b1b19e9
Ubuntu <ubuntu@ip-172-31-2-46.us-west-2.compute.internal>
2015-02-03 21:44:42 +0000

    adding flat-ui

--- build.config.js
+++ build.config.js
@@ -67,6 +67,8 @@ module.exports = {
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
       'vendor/angular-ui-utils/modules/route/route.js',
+      'vendor/flat-ui/dist/css/flat-ui.css',
+      'vendor/flat-ui/dist/js/flat-ui.min.js',
       'vendor/textAngular/dist/textAngular-rangy.min.js',
       'vendor/textAngular/dist/textAngular-sanitize.min.js',
       'vendor/textAngular/dist/textAngular.min.js',
